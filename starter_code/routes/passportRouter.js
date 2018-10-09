d2938f20f099ae42d40f8c394ee92bdfd7fa711b
Adolfo Vela <vela@tekplanner.com>
2018-10-09 16:24:33 -0500

    Listo iteracion 1

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -9,9 +9,49 @@ const ensureLogin = require("connect-ensure-login");
 const passport      = require("passport");
 
 
-
+//---GET´S----
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+
+router.get("/signup", (req,res,next)=>{
+  res.render('../views/passport/signup.hbs')
+});
+
+//POST para traer la info del signup Y SE HACEN VALIDACIONES
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const salt = bcrypt.genSaltSync(bcryptSalt);
+  const hashPass = bcrypt.hashSync(password, salt);
+
+  //Si hay campos vacios no guarda
+  if (username == "" || password == "") {
+    res.render("auth/signup", {
+      errorMessage: "Introduce usuario y contraseña"
+    });
+    return;
+  }
+
+  //SI USUARIO YA EXISTE NO DEBE GUARDAR y si si guarda
+  User.findOne({
+    "username": username
+  }, "username", (error, user) => {
+    if (user) {
+      res.render("auth/signup", {
+        errorMessage: "El usuario ya existe"
+      })
+      return
+    }
+    const newUser = User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      res.redirect("/");
+    });
+  })
+})
 
 module.exports = router;
